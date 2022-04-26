ef4c859bf3102ab69906fac2f76f8b6c44b63776
boldak <boldak.andrey@gmail.com>
2022-04-26 16:25:17 +0300

    fix index

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const Container = require("./src/container")
 const Service = require("./src/service")
 const ServiceWrapper = require("./src/service-wrapper")
-const createLogger = require("./src/logger")
-const createMonitor = require("./src/metrics")
+// const createLogger = require("./src/logger")
+// const createMonitor = require("./src/metrics")
 
 
 module.exports = {
 	Container,
 	Service,
 	ServiceWrapper,
-	createLogger,
-	createMonitor
+	// createLogger,
+	// createMonitor
 }
